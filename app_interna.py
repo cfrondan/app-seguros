66bafb765ac8b4b0ed34fef4352073f659b3516e
Claudio Rondan <cfrsegurosgenerales@gmail.com>
2026-05-02 23:40:17 -0300

    Corrijo linea 154 app interna

--- app_interna.py
+++ app_interna.py
@@ -151,9 +151,9 @@ if query:
             st.write("- " + f)
 
     if fuentes_ordenadas["otro"]:
-    st.markdown("**📄 Otros:**")
-    for f in fuentes_ordenadas["otro"]:
-        st.write("- " + f)
+        st.markdown("**📄 Otros:**")
+        for f in fuentes_ordenadas["otro"]:
+            st.write("- " + f)
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
